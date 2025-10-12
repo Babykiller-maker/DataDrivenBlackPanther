fcac9f190f9dfd94a4febd33b22850772ccf2b70
shashikantyadav287 <37799716+shashikantyadav287@users.noreply.github.com>
2025-10-12 11:48:27 +0530

    Update Sample1.java
    Changes by SKY

--- src/test/java/github/Sample1.java
+++ src/test/java/github/Sample1.java
@@ -4,5 +4,7 @@ public class Sample1 {
  public static void main(String[] args) {
 	System.out.println("hello");
 	 System.out.println("Sky");
+	 System.out.println("Sky2");
+	 System.out.println("Sky3");
 }
 }
